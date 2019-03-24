29f25ad37e890947fd97efd52bbe0e1fe995aad1
Andrew So <soandrew@users.noreply.github.com>
2019-03-24 01:05:47 -0400

    Have front end call api for category list

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,42 @@
 class App extends React.Component {
   render() {
-    return <h1>Welcome to Univity!</h1>
+    return (
+      <ReactBootstrap.Jumbotron>
+        <h1>Welcome to Univity!</h1>
+        <CategoryList />
+      </ReactBootstrap.Jumbotron>
+    );
+  }
+}
+
+class CategoryList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("/api/categories")
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          categories: result
+        });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.categories.map(category => (
+          <li key={category}>
+            {category}
+          </li>
+        ))}
+      </ul>
+    );
   }
 }
 
